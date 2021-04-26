463de8701bf3c9de78d86d13eed1b5dff81e736d
Missy <82602150+laneandkent@users.noreply.github.com>
2021-04-26 11:20:11 -0500

    Initial commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <h1>Warning! Danger ahead.</h1>
+  `,
+  styleUrls: ['./warning-alert.component.css']
+
+})
+export class WarningAlertComponent {
+
+}
